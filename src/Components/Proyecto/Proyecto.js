fdddb0191d5c6bad18865564f840407a3ba0a9f7
mauroradino <mauroradino2003@gmail.com>
2024-10-08 19:53:49 -0300

    arreglo proyectos json

--- src/Components/Proyecto/Proyecto.js
+++ src/Components/Proyecto/Proyecto.js
@@ -3,15 +3,14 @@ import "./Proyecto.css"
 
 const Proyecto = ({ imagen, titulo, subtitulo, enlace }) => {
     return (
-        <a style={{ textDecoration: "none" }} href={enlace} target="_blank" rel="noreferrer">
-            <div className="divProyecto">
-                <img className="imagenProyecto" src={imagen} alt="imagen del proyecto" />
-                <div className="textosProyecto">
-                    <p className="tituloProyecto">{titulo}</p>
-                    <p className="subtituloProyecto">{subtitulo}</p>
-                </div>
+        <div className="divProyecto">
+            <img className="imagenProyecto" src={imagen} alt="imagen del proyecto" />
+            <div className="textosProyecto">
+                <p className="tituloProyecto">{titulo}</p>
+                <p className="subtituloProyecto">{subtitulo}</p>
+                <button onClick={() => window.open(enlace, '_blank')} className="verBtn">Ver</button>
             </div>
-        </a>
+        </div>
     )
 }
 
--- src/Components/Proyectos/Proyectos.js
+++ src/Components/Proyectos/Proyectos.js
@@ -3,16 +3,20 @@ import Proyecto from "../Proyecto/Proyecto"
 import "./Proyectos.css"
 
 import imagenProyecto1 from "../../Assets/imagenProyecto1.png";
-
-
+import imagenProyecto2 from "../../Assets/imagenProyecto2.png"
+import imagenProyecto3 from "../../Assets/imagenProyecto3.png"
+import imagenProyecto4 from "../../Assets/imagenProyecto4.png"
 const imageMap = {
     "imagenProyecto1.png": imagenProyecto1,
+    "imagenProyecto2.jepg": imagenProyecto2,
+    "imagenProyecto3.png": imagenProyecto3,
+    "imagenProyecto4.png": imagenProyecto4
 };
 
 
 const Proyectos = () => {
     return (
-        <div className="divProyectos">
+        <div id="proyectos" className="divProyectos">
             <h2 className="tituloProyectos">Proyectos</h2>
             {ProyectosJSON.map((proyecto) => {
                 return (
